32fa4ed3a7bdbd70369bda3142e4b0f896c747f9
marclllaks <61365408+marclllaks@users.noreply.github.com>
2021-04-02 16:23:54 +0200

    Working alpha version

---
+++ main.py
@@ -0,0 +1,90 @@
+import asyncio
+import json
+import os
+
+import discord
+from dotenv import load_dotenv
+from notion.client import NotionClient
+
+#Discord init
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
+
+#Notion init
+NTOKEN = os.getenv('NOTION_TOKEN')
+NCALENDAR = os.getenv('NOTION_CALENDAR')
+nclient = NotionClient(token_v2=NTOKEN)
+
+tablazat = nclient.get_collection_view(NCALENDAR)
+
+
+server=0
+client = discord.Client()
+
+def getEvents(table):
+    return table.collection.get_rows()
+
+def saveEvents(events):
+    with open('events.json', 'w+') as f:
+        f.write(json.dumps(events))
+
+def loadEvents():
+    with open('events.json', 'r') as f:
+        return json.loads(f.read())
+
+def formatEvent(event):
+    pass
+
+events=loadEvents()
+
+async def my_background_task():
+    while True:
+        await asyncio.sleep(3)
+        await client.wait_until_ready()
+        if server:
+            newEvents=getEvents(tablazat)
+            for event in newEvents:
+                if event.Reset_send:
+                    try:
+                        events.remove(event.id)
+                        saveEvents(events)
+                    except:
+                        pass
+                    event.Reset_send=False
+                    event.Send_reminder=False
+
+                if (event.id not in events) and event.Send_reminder and event.date:
+                    for tag in event.tags:
+                        try:
+                            channel = discord.utils.get(server.text_channels, name=tag)
+                            print("Új event!", event)
+                            embed=discord.Embed(color=0xcd2323)
+                            embed.add_field(name=event.title, value=event.date.start.strftime("%Y. %m. %d.\n%H:%M"), inline=True)
+                            embed.set_footer(text=', '.join(map(str,event.tags)))
+                            await channel.send(embed=embed)
+                            events.append(event.id)
+                            saveEvents(events)
+                        except:
+                            pass
+
+
+
+
+@client.event
+async def on_ready():
+    global server
+    for guild in client.guilds:
+        if guild.name == GUILD:
+            server=guild
+
+    print(
+        f'{client.user} is connected to the following guild:\n'
+        f'{guild.name}(id: {guild.id})'
+    )
+
+
+client.loop.create_task(my_background_task())
+client.run(TOKEN)
+
+
